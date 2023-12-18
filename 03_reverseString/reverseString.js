16e3fccfddd1b589e664b2f84e4c738599f2ac33
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2023-12-18 10:46:44 +0000

    feat : reverseString

--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,6 +1,11 @@
-const reverseString = function() {
-
+const reverseString = function(string) {
+    let part = "";
+    for (let i = string.length-1; i >= 0 ;i--) {
+        part += string.slice(i, i+1);
+    }
+    return part;
 };
+
 
 // Do not edit below this line
 module.exports = reverseString;
