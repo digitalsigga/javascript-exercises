d0abf480457adbe80b40c85686c36d208ab719ac
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2023-12-18 12:42:09 +0000

    feat : arrays

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,6 +1,24 @@
-const removeFromArray = function() {
 
+
+const removeFromArray = function(array, ...args) {
+    
+    const newArr = [];
+
+    array.forEach((item) => {
+        if (!args.includes(item)) {
+            newArr.push(item);
+        }
+    });
+    console.log(newArr);
+    return newArr;
 };
+
+let arrHe = ["hello", "hi", "you"];
+
+removeFromArray(arrHe, "hi", "you");
+
+
 
 // Do not edit below this line
 module.exports = removeFromArray;
+
