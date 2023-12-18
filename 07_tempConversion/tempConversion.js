6642e7fcde805c8bd352c076b5d79f69efd8bf83
Sigrídur Birna Matthíasdóttir <sigridurbirnamatthiasdottir@Sigridurs-MacBook-Pro.local>
2023-12-18 13:38:43 +0000

    feat : tempConversion finished

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function(numFahr) {
+  let numCels = (numFahr - 32) * (5/9); 
+  numCels = parseFloat(numCels.toFixed(1)); 
+  return numCels
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function(numCels) {
+  let numFahr = numCels * (9/5) + 32;
+  numFahr = parseFloat(numFahr.toFixed(1));
+  return numFahr
 };
+
+convertToCelsius(100);
 
 // Do not edit below this line
 module.exports = {
